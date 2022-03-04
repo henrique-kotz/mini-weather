7cddb1ef1db048b5349f0b65676899cdd6231d12
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-03-04 19:49:56 -0300

    display weather info

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,15 +15,30 @@ function getLocation() {
 }
 
 function getCurrentWeather(lat, long) {
-    const promise = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`);
+    const promise = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}&lang=pt_br`);
 
-    promise.then(response => {
-        console.log(response.data);
-    });
+    promise.then(response => renderWeather(response.data));
 
     promise.catch(error => {
         console.log(error.response.status);
     });
+}
+
+function renderWeather(data) {
+    console.log(data)
+    const bannerInfo = document.querySelector('.current-weather .banner');
+    bannerInfo.innerHTML = `
+        <h2>${data.weather[0].description}</h2>
+    `;
+
+    const weatherData = document.querySelector('.current-weather .weather-data');
+    weatherData.innerHTML = `
+        <p>Temperatura: ${data.main.temp}</p>
+        <p>Sensação Térmica: ${data.main.feels_like}</p>
+        <p>Umidade do ar: ${data.main.humidity}%</p>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="weather icon">
+        <p>Cidade: ${data.name}</p>
+    `;
 }
 
 function manualWeather() {

02965b05186f9117b386f94dc9726454132d3759
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-03-04 19:15:51 -0300

    get location from user

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,33 @@
-alert('opa');
+const API_KEY = '96cab56924eb6ba04b1a50c65075fd03';
+
+function checkIfGeolocation() {
+    if('geolocation' in navigator) {
+        getLocation();
+      } else {
+        manualWeather();
+      }
+}
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        getCurrentWeather(position.coords.latitude, position.coords.longitude);
+    }, (error) => console.log(error));
+}
+
+function getCurrentWeather(lat, long) {
+    const promise = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`);
+
+    promise.then(response => {
+        console.log(response.data);
+    });
+
+    promise.catch(error => {
+        console.log(error.response.status);
+    });
+}
+
+function manualWeather() {
+    alert('Por favor, ṕreencha os dados para obter as informações do tempo :D');
+}
+
+checkIfGeolocation();
